510ea59bf061397c9f101db97cceef8d20bf6bdc
Gustavo-Vargas06 <gustavovargas@unomaha.edu>
2025-02-05 16:20:49 +0000

    In Progress

--- RPS.py
+++ RPS.py
@@ -8,14 +8,30 @@ def main():
   wins = 0
   ties = 0
   losses = 0
-  #Create a loop that continues as long as the user wants to play.
-  #User can play as many games as they wish.
-
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
-
+ #Create a loop that continues as long as the user wants to play.
+ #User can play as many games as they wish.
+  play = "Y"
+  while play == "Y":
+#Randomly choose the computer between 'R', 'P', or 'S'
+    player = input("Chooese between (R/P/S): ")
+    computer = random.choice(["R", "P", "S"])
+    print("I choose", computer)
+    #Prompt the user for their RPS selection
+    
+#Determine winner and state what happened to the user
+   
+    if player == computer:
+      print("Tie")
+      ties = ties + 1
+    elif player > computer:
+      print("You Won!!!")
+      wins = wins + 1
+    elif player < computer:
+      print("You Suck! ToT")
+      losses = losses + 1
+#Ask the user if they would like to play again.
+    play = input("Play again? (Y/N): ")
+  
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
   print("---- \t ---- \t ------")
